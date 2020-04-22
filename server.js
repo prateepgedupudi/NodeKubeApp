6906576d8a4e762bad8130cdacdce8990c9eef1e
Prateep Gedupudi <prateep.gedupudi@gmail.com>
2020-04-22 23:40:01 +0530

    Added local IP in the response to prove loadbalancer concept with kubernetes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var ip = require('ip');
 
 // Constants
 const PORT = 8080;
@@ -9,8 +10,9 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(`Hello World! I am responding from ${ip.address()}`);
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`My internal IP is: ${ip.address()}`);
